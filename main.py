52612282bb92bd29fa120ebb9038951e126d0601
prasad3366 <prasaddurga7463@gmail.com>
2025-11-20 14:43:24 +0530

    change3

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ app = Flask(__name__, static_folder='../frontend')
 CORS(app, resources={
     r"/*": {
         "origins": [
-            "http://localhost:5174",
-            "http://127.0.0.1:5174"
+            "http://localhost:5173",
+            "http://127.0.0.1:5173"
         ],
         "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         "allow_headers": ["Content-Type", "Authorization"],
